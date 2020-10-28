3b91bb30a2a300e34b8a36a958b271b522854933
benjagonzaleznavarrete <benja030689gmail.com>
2020-10-28 18:14:39 -0300

    Initial commit

---
+++ app/src/main/java/com/example/banco_ev02/info_act.java
@@ -0,0 +1,14 @@
+package com.example.banco_ev02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class info_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_act);
+    }
+}
---
+++ app/src/main/java/com/example/banco_ev02/seguridad_act.java
@@ -0,0 +1,14 @@
+package com.example.banco_ev02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class seguridad_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seguridad_act);
+    }
+}
