12ba525f70d5bd1f0736bdce3c09f6c9271ac5a8
DiegoMuss <71735509+DiegoMuss@users.noreply.github.com>
2020-10-28 18:27:46 -0300

    Add files via upload
    comentado 2

--- app/src/main/java/com/example/banco_ev02/clientes_act.java
+++ app/src/main/java/com/example/banco_ev02/clientes_act.java
@@ -10,5 +10,7 @@ public class clientes_act extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clientes_act);
+
+        //hola puto
     }
 }
